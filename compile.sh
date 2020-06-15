a56a624dcb96825ac4ed6aac3e7a75bd9458e133
Leo Le Bouter <lle-bout@zaclys.net>
2020-06-16 00:48:16 +0200

    Remove prebuilt binary, advice using GNU/Linux distro packages

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -client -jar closure-compiler.jar \
+closure-compiler \
   --js js-lib/**.js \
   --js js-lib/**/*.js \
   --warning_level=VERBOSE --formatting=PRETTY_PRINT --language_in=ECMASCRIPT6 --compilation_level=ADVANCED_OPTIMIZATIONS \
